c4b00aec824291b35dcd75bc4289291ce60f508c
Aleyucra74 <alexandre.yucra@outlook.com>
2019-07-24 12:11:20 -0300

    Validando os campos

--- contatos.json/index.php
+++ contatos.json/index.php
@@ -2,6 +2,20 @@
     //definindo uma constante para o nome do arquivo
     define('ARQUIVO','contatos.json');
 
+    //funcao para validar dados do post
+    function errosNoPost(){
+        $erros = [];
+        if(!isset($_POST['nome']) || $_POST['nome']=='' ){
+            $erros[] = 'errNome';
+        }
+        if(!isset($_POST['email']) || $_POST['email']=='' ){
+            $erros[] = 'errEmail';
+        }
+
+        return $erros;
+    }
+
+
     //carregando o conteudo do arquivo(string json) para a variavel
     function getContatos(){
         $json = file_get_contents(ARQUIVO);
@@ -27,18 +41,25 @@
         file_put_contents(ARQUIVO,$json);
     }
 
-    if (!$_POST == '') {
-        # code...
-        echo "preencha os campos";
-    } else {
+    // if (!$_POST == '') {
+    //     # code...
+    //     echo "preencha os campos";
+    // } else {
+        $erros = errosNoPost();
 
         if($_POST){
-            //add o contato ao arquivo json
-            addContatos($_POST['nome'],$_POST['email']);
+            //verificando o post
 
+            if(count($erros) ==0){
+                //add o contato ao arquivo json
+                addContatos($_POST['nome'],$_POST['email']);
+            }
+
+        }else {
+            $erros = [];
         }
 
-    }
+    //}
     $contatos = getContatos();
 
 ?>
@@ -65,7 +86,7 @@
 
 
     <form action="index.php" method="POST">
-        <input type="text" name="nome" id="nome" placeholder="digite o nome">
+        <input <?php echo (in_array('errNome',$erros)?'style="border:red 1px solid"' : ''); ?> type="text" name="nome" id="nome" placeholder="digite o nome">
         <input type="email" name="email" id="email" placeholder="digite o email">
         <button type="submit">Salvar</button>
     </form>
